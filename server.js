c755d9c66025c5950f8697208b6aa9ad268de5cf
bhavya srivastava <bhavyasrivastava182@gmail.com>
2024-02-22 00:18:57 +0530

    changes

--- server.js
+++ server.js
@@ -13,6 +13,21 @@ app.use((req, res, next) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+//-----------deployment
+const __dirname1 =path.resolve();
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname1,"/build")));
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname1,"build", "index.html"))
+    })
+    
+}else{
+    app.get('/',(req,res)=>{
+        res.send("API is Running Successfully")
+    });
+}
+
 const userSocketMap = {};
 function getAllConnectedClients(roomId) {
     // Map
